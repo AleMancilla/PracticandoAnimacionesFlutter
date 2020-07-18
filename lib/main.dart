b01fd5e12bdd323112e2f6c121f3fbb69cb0010a
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-07-17 23:10:20 -0400

    pageAnimationNotification

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:animated_do_app/src/pages/NavegacionPage.dart';
 import 'package:animated_do_app/src/pages/Pagina1_page.dart';
-import 'package:animated_do_app/src/pages/twitterPage.dart';
+// import 'package:animated_do_app/src/pages/twitterPage.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      home: Pagina1_page(),
+      home: NavegacionPage(),
     );
   }
 }
--- lib/src/pages/NavegacionPage.dart
+++ lib/src/pages/NavegacionPage.dart
@@ -1,10 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class NavegacionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: null,
+      appBar: AppBar(
+        title: Text("Notification page"),
+        backgroundColor: Colors.pink,
+      ),
+      floatingActionButton: BotonFlotante(),
+      bottomNavigationBar: BottomNavigation(),
+    );
+  }
+}
+
+class BottomNavigation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      selectedItemColor: Colors.pink,
+      items: [
+        BottomNavigationBarItem(
+          title: Text('Bones'),
+          icon: FaIcon(FontAwesomeIcons.bone)
+        ),
+        
+        BottomNavigationBarItem(
+          title: Text('Notifications'),
+          icon: Stack(
+            children: [
+              FaIcon(FontAwesomeIcons.bell),
+              Positioned(
+                top: 0.0,
+                right: 0.0,
+                child: Container(
+                  child: Text("1",style: TextStyle(color: Colors.white,fontSize: 7.0,),),
+                  alignment: Alignment.center,
+                  width: 12.0,
+                  height: 12.0,
+                  decoration: BoxDecoration(
+                    color: Colors.redAccent,
+                    shape: BoxShape.circle
+                  ),
+                )
+              )
+            ],
+          )
+        ),
+        
+        BottomNavigationBarItem(
+          title: Text('My Dog'),
+          icon: FaIcon(FontAwesomeIcons.dog)
+        ),
+        
+      ],
+    );
+  }
+}
+
+class BotonFlotante extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: (){},
+      backgroundColor: Colors.pink,
+      child: FaIcon(FontAwesomeIcons.play),
     );
   }
 }
